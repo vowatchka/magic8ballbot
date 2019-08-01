9d66cdc166498991109fb4963403e79e45f99c3b
vowatchka <vowatchka@mail.ru>
2019-08-01 14:39:42 +0300

    change interval of bot

--- magic8ballbot/__init__.py
+++ magic8ballbot/__init__.py
@@ -121,7 +121,7 @@ def get_message(message):
 	
 keyboard = create_lang_keyboard()
 try:
-	magic8ballbot.polling(none_stop=True, interval=0)
+	magic8ballbot.polling(none_stop=True, interval=10)
 except Exception as ex:
 	time.sleep(5)
 	print("Internet error!")
